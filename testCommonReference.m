4d401cb18db9199bd18c48e1cac7103c2fcee2e1
Mahtab Tehrani <mtehrani@neomed.edu>
2023-05-11 13:09:18 -0400

    cleaned up testCommonReference

--- testCommonReference.m
+++ testCommonReference.m
@@ -52,7 +52,15 @@ dpath = '/media/sshanbhag/SSData/Data/Mouse/Raw/BLA/1500/20230213';
 dname = '1500_20230213_01_0_3352_BBN.dat';
 tname = '1500_20230213_01_0_3352_BBN_testdata.mat';
 
-% define path to data file and return as OptoFileName object
+
+%------------------------------------------------------------------------
+%% specify channel to export
+%------------------------------------------------------------------------
+exportChannel = 1;
+
+%------------------------------------------------------------------------
+%% define path to data file and return as OptoFileName object
+%------------------------------------------------------------------------
 F = defineSampleData({dpath}, {dname}, {tname});
 % determine # of files
 nFiles = length(F);
@@ -74,8 +82,6 @@ NexFilePath = pwd;
 Channels = 1:16;
 nChannels = length(Channels);
 
-% specify channel to export
-exportChannel = 1;
 BPfilt = buildFilter;
 resampleData = [];
 [cSweepsRaw, nexInfo] = read_data_for_export(F, Channels, ...
@@ -101,7 +107,6 @@ nexInfo.InfoFileName = fullfile(NexFilePath, [baseName '_nexinfo.mat']);
 cSweepsAvg = applyCommonReference(cSweepsRaw, @common_avg_ref);
 cSweepsMed = applyCommonReference(cSweepsRaw, @common_med_ref);
 
-%{
 % copy of cSweepsRaw for storing avg ref data
 cSweepsAvg = cSweepsRaw;
 % loop through files
@@ -139,36 +144,6 @@ for f = 1:nFiles
       cSweepsMed{f}(:, s) = c;
    end
 end
-%}
-
-%------------------------------------------------------------------------
-%------------------------------------------------------------------------
-%% some diagnostics - plot raw, avg, med ref data
-%------------------------------------------------------------------------
-%------------------------------------------------------------------------
-%{
-for f = 1:nFiles
-   nSweeps = size(cSweepsRaw{f}, 2);
-   % loop through sweeps (aka trials)
-   for s = 1:nSweeps
-      if f == 1 && s == 1
-         fig = plotRawAvgMedRef([], ...
-                                 cSweepsRaw{f}(:, s), ...
-                                 cSweepsAvg{f}(:, s), ...
-                                 cSweepsMed{f}(:, s), ...
-                                 nexInfo.Fs);
-      else
-         plotRawAvgMedRef(fig, ...
-                                 cSweepsRaw{f}(:, s), ...
-                                 cSweepsAvg{f}(:, s), ...
-                                 cSweepsMed{f}(:, s), ...
-                                 nexInfo.Fs);
-      end
-      fig.Name = sprintf('%s_Trial%d', F.base, s);
-      pause
-   end
-end
-%}
 
 %------------------------------------------------------------------------
 %------------------------------------------------------------------------
@@ -309,85 +284,6 @@ writeNexFile(nD, nexInfo.FileName);
 sendmsg(sprintf('Writing _nexinfo.mat file %s:', ...
 											nexInfo.InfoFileName));
 save(nexInfo.InfoFileName, 'nexInfo', '-MAT');
-
-%------------------------------------------------------------------------
-%------------------------------------------------------------------------
-%% process data for single trial
-%------------------------------------------------------------------------
-%------------------------------------------------------------------------
-% this section was used during initial testing
-%{
-% read data without filtering, resampling
-% [cSweepsRaw, nexInfo] = read_data_for_export(F, Channels, [], []);
-
-% define filter using internal function
-BPfilt = buildFilter(nexInfo.Fs);
-
-% get stimulus information
-onset = nexInfo.FileInfo{1}.Dinf.audio.Delay;
-offset = onset + nexInfo.FileInfo{1}.Dinf.audio.Duration;
-
-% % set up raw data plot
-% [r.fH, ar.X, r.pH] = tracePlot(cell2mat(cSweepsRaw{1}(:, 1))', ...
-%                                                                nexInfo.Fs);
-% r.fH.Name = 'RAW';
-% % set up avg data plot
-% [a.fH, a.aX, a.pH] = tracePlot(cell2mat(cSweepsRaw{1}(:, 1))', ...
-%                                                                nexInfo.Fs);
-% a.fH.Name = 'AVG';
-% % set up med data plot
-% [m.fH, m.aX, m.pH] = tracePlot(cell2mat(cSweepsRaw{1}(:, 1))', ...
-%                                                                nexInfo.Fs);
-% m.fH.Name = 'MED';
-
-% select trial
-trialN = 100;
-D = cSweepsRaw{1}(:, trialN);
-
-% filter the data
-for c = 1:length(D)
-   D{c} = filtfilt(BPfilt.b, BPfilt.a, ...
-							sin2array(D{c}, nexInfo.Fs, BPfilt.ramp));
-end
-
-% transform cell array to matrix with time bins in columns, channels by rows
-T = cell2mat(D);
-% apply common average referencing to data
-Ta = common_avg_ref(T);
-% apply common median referencing to data
-Tm = common_med_ref(T);
-
-% Plot data
-figure(1)
-tl = tiledlayout(1, 3);
-tl.TileSpacing = 'compact';
-tl.Padding = 'compact';
-
-% plot raw data
-nexttile
-[r.pH, r.aX, r.fH] = tracePlot(T', nexInfo.Fs);
-title(r.aX, 'RAW');
-% draw onset/offset lines
-line([onset onset], r.aX.YLim, 'Color', 'g');
-line([offset offset], r.aX.YLim, 'Color', 'r');
-
-% plot avg data
-nexttile
-[a.pH, a.aX, a.fH] = tracePlot(Ta', nexInfo.Fs);
-title(a.aX, 'AVG');
-line([onset onset], r.aX.YLim, 'Color', 'g');
-line([offset offset], r.aX.YLim, 'Color', 'r');
-% plot med data
-nexttile;
-[m.pH, m.aX, m.fH] = tracePlot(Tm', nexInfo.Fs);
-title(m.aX, 'MED');
-line([onset onset], r.aX.YLim, 'Color', 'g');
-line([offset offset], r.aX.YLim, 'Color', 'r');
-
-r.fH.Name = sprintf('%s_Sweep%d', F.base, trialN);
-r.fH.FileName = sprintf('%s_Sweep%d', F.base, trialN);
-r.fH.PaperOrientation = 'landscape';
-%}
 
 
 %------------------------------------------------------------------------
